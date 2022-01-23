9211fb9e2b8c6c62f7069e5609e6069103980334
Joao Souza <joao_suza@outlook.com>
2022-01-23 19:23:30 -0300

    Update build.gradle.kts adding some dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,13 +18,16 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter:2.6.2")
 	implementation("org.springframework.boot:spring-boot-starter-web:2.6.2")
+	implementation("org.springframework.boot:spring-boot-starter-validation:2.6.2")
 	implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.2")
 	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
 	testImplementation("io.mockk:mockk:1.12.2")
+	implementation("io.rest-assured:kotlin-extensions:4.5.0")
 }
 
 tasks.withType<KotlinCompile> {
