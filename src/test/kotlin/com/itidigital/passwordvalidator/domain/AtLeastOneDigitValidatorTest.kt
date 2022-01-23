a929c9fc58ddeed7b5bcb089d89b041c331acf72
Joao Souza <joao_suza@outlook.com>
2022-01-23 11:48:41 -0300

    Update AtLeastOneDigitValidatorTest.kt

--- src/test/kotlin/com/itidigital/passwordvalidator/domain/AtLeastOneDigitValidatorTest.kt
+++ src/test/kotlin/com/itidigital/passwordvalidator/domain/AtLeastOneDigitValidatorTest.kt
@@ -11,48 +11,52 @@ class AtLeastOneDigitValidatorTest {
     @Test
     fun `given an input string when it ends with a number should return true`(){
 
+        //arrange
         val input = "ab c123"
 
-
+        //act
         val result = validator.meetsRequirement(input)
 
-
+        //assert
         assertTrue(result)
     }
 
     @Test
     fun `given an input string when it starts with a number should return true`(){
 
+        //arrange
         val input = "12ab\$c"
 
-
+        //act
         val result = validator.meetsRequirement(input)
 
-
+        //assert
         assertTrue(result)
     }
 
     @Test
     fun `given an input string when it contains a number should return true`(){
 
+        //arrange
         val input = "ab2c"
 
-
+        //act
         val result = validator.meetsRequirement(input)
 
-
+        //assert
         assertTrue(result)
     }
 
     @Test
     fun `given an input string when it doesn't contains a number should return false`(){
 
+        //arrange
         val input = "ab c "
 
-
+        //act
         val result = validator.meetsRequirement(input)
 
-
+        //assert
         assertFalse(result)
     }
 }
